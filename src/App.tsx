70d5226ff8b0eae3fecfa068f858e939c87bb09a
Tim Iles <tim_iles@live.com>
2017-11-24 22:12:58 +0000

    some styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,16 @@
 import * as React from 'react';
 import './App.css';
 
-const logo = require('./logo.svg');
-
 class App extends React.Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+      <div className="app">
+        <div className="app-header">
+          <h2>Simon</h2>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.tsx</code> and save to reload.
-        </p>
+        <div className="app-container">
+          Hello world!
+        </div>
       </div>
     );
   }
