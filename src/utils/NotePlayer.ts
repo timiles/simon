84150754ad6deab951d26fc04961c1db66491b39
Tim Iles <tim_iles@live.com>
2017-12-24 11:13:00 +0000

    use setValueAtTime (value setter is deprecated); also check pitch > 0

--- src/utils/NotePlayer.ts
+++ src/utils/NotePlayer.ts
@@ -2,19 +2,22 @@ import FrequencyUtils from './FrequencyUtils';
 import Note from '../Note';
 
 export default class NotePlayer {
-  constructor(private audioContext: AudioContext) {}
+  constructor(private audioContext: AudioContext) { }
 
   play(note: Note, startSecondsFromNow: number = 0) {
-    const gainNode = this.audioContext.createGain();
-    gainNode.gain.value = 0.1;
-    gainNode.connect(this.audioContext.destination);
+    if (note.pitch > 0) {
+      const gainNode = this.audioContext.createGain();
+      gainNode.gain.setValueAtTime(0.1, this.audioContext.currentTime);
+      gainNode.connect(this.audioContext.destination);
 
-    const oscillator = this.audioContext.createOscillator();
-    oscillator.frequency.value = FrequencyUtils.getFrequencyFromPitch(note.pitch);
-    oscillator.connect(gainNode);
+      const oscillator = this.audioContext.createOscillator();
+      const frequency = FrequencyUtils.getFrequencyFromPitch(note.pitch);
+      oscillator.frequency.setValueAtTime(frequency, this.audioContext.currentTime);
+      oscillator.connect(gainNode);
 
-    const startWhen = startSecondsFromNow + this.audioContext.currentTime;
-    oscillator.start(startWhen);
-    oscillator.stop(startWhen + note.durationSeconds);
+      const startWhen = startSecondsFromNow + this.audioContext.currentTime;
+      oscillator.start(startWhen);
+      oscillator.stop(startWhen + note.durationSeconds);
+    }
   }
 }
